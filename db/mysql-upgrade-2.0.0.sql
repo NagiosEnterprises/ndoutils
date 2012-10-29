ad1d74cebda0d6735e932f7df6524a157e9bb9f1
mguthrie88 <mguthrie88@users.sourceforge.net>
2012-10-29 18:53:57 +0000

    Fixes for new table indexes

--- db/mysql-upgrade-2.0.0.sql
+++ db/mysql-upgrade-2.0.0.sql
@@ -1,8 +1,8 @@
 -- BEGIN 2.0 MODS 
 
-ALTER TABLE `nagios_notifications ADD INDEX (`start_time`);
-ALTER TABLE `nagios_contactnotifications ADD INDEX (`start_time`);
-ALTER TABLE `nagios_contactnotificationmethods ADD INDEX (`start_time`);
+ALTER TABLE `nagios_notifications` ADD INDEX (`start_time`);
+ALTER TABLE `nagios_contactnotifications` ADD INDEX (`start_time`);
+ALTER TABLE `nagios_contactnotificationmethods` ADD INDEX (`start_time`);
 ALTER TABLE `nagios_logentries` ADD INDEX (`logentry_time`); 
 ALTER TABLE `nagios_acknowledgements` ADD INDEX (`entry_time`); 
 
