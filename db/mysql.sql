NagiosEnterprises/ndoutils

commit bc6088e0dc91f5fe01e023bf1f2e9732a6d438f9

Patch situation where db table nagios_logentries has items where
`instance_id`,`logentry_time`,`entry_time`,`entry_time_usec`
isn't unique enough.

db/mysql.sql
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `nagios_acknowledgements` (
   `persistent_comment` smallint(6) NOT NULL default '0',
   `notify_contacts` smallint(6) NOT NULL default '0',
   PRIMARY KEY  (`acknowledgement_id`),
-  UNIQUE KEY `instance_id` (`instance_id`, `entry_time`, `entry_time_usec`)
+  UNIQUE KEY `instance_id` (`instance_id`, `entry_time`, `entry_time_usec`, `logentry_id`)
 ) ENGINE=MyISAM COMMENT='Current and historical host and service acknowledgements';
 
 -- --------------------------------------------------------

db/mysql-upgrade-2.1.0.sql
@@ -1,0 +1,15 @@
+-- BEGIN 2.0.1 MODS 
+
+set @exist := (select count(*) from information_schema.statistics where table_name = 'nagios_logentries' and index_name = 'instance_id');
+set @sqlstmt := if( @exist > 0, 'ALTER TABLE `nagios_logentries` DROP KEY `instance_id`', 'select ''INFO: Index does not exists.''');
+PREPARE stmt FROM @sqlstmt;
+EXECUTE stmt;
+
+ALTER TABLE `nagios_logentries` ADD UNIQUE KEY `instance_id` (`instance_id`,`logentry_time`,`entry_time`,`entry_time_usec`,`logentry_id`);
+
+-- --------------------------------------------------------
+
+--
+
+-- END 2.0.1 MODS 
+

db/upgradedb
@@ -60,7 +60,7 @@ if ($@) {
 # version is *not* necessarily the same as the software version. Also for
 # version prior to 2.0.1, the schema version was the same as the software
 # version and there may not be an upgrade file.
-my @schemaversions = ( "1.4b2", "1.4b3", "1.4b4", "1.4b5", "1.4b6", "1.4b7", "1.4b8", "1.4b9", "1.5", "1.5.1", "1.5.2", "2.0.0", "2.0.1" );
+my @schemaversions = ( "1.4b2", "1.4b3", "1.4b4", "1.4b5", "1.4b6", "1.4b7", "1.4b8", "1.4b9", "1.5", "1.5.1", "1.5.2", "2.0.0", "2.0.1", "2.1.0" );
 # Get current database version
 my $version;
 my $legacyversion = $schemaversions[0];

commit c2838d5a13ede4befda023ae6d7e2e7433c53d64

Fix bug introduced with bc6088e0dc91f5fe01e023bf1f2e9732a6d438f9

db/mysql.sql
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `nagios_acknowledgements` (
   `persistent_comment` smallint(6) NOT NULL default '0',
   `notify_contacts` smallint(6) NOT NULL default '0',
   PRIMARY KEY  (`acknowledgement_id`),
-  UNIQUE KEY `instance_id` (`instance_id`, `entry_time`, `entry_time_usec`, `logentry_id`)
+  UNIQUE KEY `instance_id` (`instance_id`, `entry_time`, `entry_time_usec`)
 ) ENGINE=MyISAM COMMENT='Current and historical host and service acknowledgements';
 
 -- --------------------------------------------------------
@@ -835,7 +835,7 @@ CREATE TABLE IF NOT EXISTS `nagios_logentries` (
   `realtime_data` smallint(6) NOT NULL default '0',
   `inferred_data_extracted` smallint(6) NOT NULL default '0',
   PRIMARY KEY  (`logentry_id`),
-  UNIQUE KEY `instance_id` (`instance_id`, `logentry_time`,`entry_time`,`entry_time_usec`)
+  UNIQUE KEY `instance_id` (`instance_id`, `logentry_time`,`entry_time`,`entry_time_usec`, `logentry_id`)
 ) ENGINE=MyISAM COMMENT='Historical record of log entries';
 
 -- --------------------------------------------------------