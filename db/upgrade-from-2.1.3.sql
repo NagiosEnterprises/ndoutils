28a92fb979e348d740e3a557a152c13b3e49f026
madlohe <swolf@nagios.com>
2020-01-29 15:31:31 -0600

    Fix mistake in upgrade sql

--- db/upgrade-from-2.1.3.sql
+++ db/upgrade-from-2.1.3.sql
@@ -144,7 +144,7 @@ UPDATE `nagios_objects` SET `name2` = '' WHERE `name2` IS NULL;
 ALTER TABLE `nagios_objects` MODIFY `name1` varchar(1023) NOT NULL default '';
 ALTER TABLE `nagios_objects` MODIFY `name2` varchar(1023) NOT NULL default '';
 ALTER TABLE `nagios_objects` DROP INDEX `objecttype_id`;
-ALTER TABLE `nagios_objects` ADD KEY `uniq_object` (`object_id`,`name1`(162),`name2`(162));
+ALTER TABLE `nagios_objects` ADD KEY `uniq_object` (`objecttype_id`,`name1`(162),`name2`(162));
 
 ALTER TABLE `nagios_processevents` MODIFY `event_time` datetime NOT NULL default '1970-01-01 00:00:01';
 ALTER TABLE `nagios_processevents` MODIFY `program_name` varchar(16) NOT NULL default '';
